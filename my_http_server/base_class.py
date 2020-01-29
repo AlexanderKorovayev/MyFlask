dba6a2284d96596192576536553c6c0569f8ea92
av.korovaev <av.korovaev@tensor.ru>
2020-01-29 10:52:45 +0300

    добавил обработку заголовков

--- my_http_server/base_class.py
+++ my_http_server/base_class.py
@@ -1,6 +1,9 @@
 import socket
 import sys
-
+from email.parser import Parser
+from functools import lru_cache
+from urllib.parse import parse_qs, urlparse
+import json
 
 class MyHTTPServer:
 
@@ -11,6 +14,7 @@ class MyHTTPServer:
         self._host = host_name
         self._port = port_id
         self._server_name = server_name
+        self._users = {}
 
     def serve_forever(self):
         serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
@@ -39,12 +43,18 @@ class MyHTTPServer:
             self.send_error(conn, e)
 
         if conn:
-          conn.close()
+            conn.close()
 
     def parse_request(self, conn):
         rfile = conn.makefile('rb')
         method, target, ver = self.parse_request_line(rfile)
         headers = self.parse_headers(rfile)
+        host = headers.get('Host')
+        if not host:
+            raise Exception('Bad request')
+        if host not in (self._server_name,
+                        f'{self._server_name}:{self._port}'):
+            raise Exception('Not found')
         return Request(method, target, ver, headers, rfile)
 
     def parse_headers(self, rfile):
@@ -60,7 +70,8 @@ class MyHTTPServer:
             headers.append(line)
             if len(headers) > MyHTTPServer.MAX_HEADERS:
                 raise Exception('Too many headers')
-        return headers
+            str_headers = b''.join(headers).decode('iso-8859-1')
+        return Parser().parsestr(str_headers)
 
     def parse_request_line(self, rfile):
         raw = rfile.readline(MyHTTPServer.MAX_LINE + 1)  # эффективно читаем строку целиком
@@ -80,7 +91,53 @@ class MyHTTPServer:
         return method, target, ver
 
     def handle_request(self, req):
-        pass  # TODO: implement me
+        if req.path == '/users' and req.method == 'POST':
+            return self.handle_post_users(req)
+
+        if req.path == '/users' and req.method == 'GET':
+            return self.handle_get_users(req)
+
+        if req.path.startswith('/users/'):
+            user_id = req.path[len('/users/'):]
+            if user_id.isdigit():
+                return self.handle_get_user(req, user_id)
+
+        raise Exception('Not found')
+
+    def handle_post_users(self, req):
+        user_id = len(self._users) + 1
+        self._users[user_id] = {'id': user_id,
+                                'name': req.query['name'][0],
+                                'age': req.query['age'][0]}
+        return Response(204, 'Created')
+
+    def handle_get_users(self, req):
+        accept = req.headers.get('Accept')
+        if 'text/html' in accept:
+            content_type = 'text/html; charset=utf-8'
+            body = '<html><head></head><body>'
+            body += f'<div>Пользователи ({len(self._users)})</div>'
+            body += '<ul>'
+            for u in self._users.values():
+                body += f'<li>#{u["id"]} {u["name"]}, {u["age"]}</li>'
+            body += '</ul>'
+            body += '</body></html>'
+
+        elif 'application/json' in accept:
+            content_type = 'application/json; charset=utf-8'
+            body = json.dumps(self._users)
+
+        else:
+            # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/406
+            return Response(406, 'Not Acceptable')
+
+        body = body.encode('utf-8')
+        headers = [('Content-Type', content_type),
+                   ('Content-Length', len(body))]
+        return Response(200, 'OK', headers, body)
+
+    def handle_get_user(self, req, user_id):
+        pass
 
     def send_response(self, conn, resp):
         pass  # TODO: implement me
@@ -97,13 +154,35 @@ class Request:
         self.headers = headers
         self.rfile = rfile
 
+    @property
+    def path(self):
+        return self.url.path
+
+    @property
+    @lru_cache(maxsize=None)
+    def query(self):
+        return parse_qs(self.url.query)
+
+    @property
+    @lru_cache(maxsize=None)
+    def url(self):
+        return urlparse(self.target)
+
+
+class Response:
+    def __init__(self, status, reason, headers=None, body=None):
+        self.status = status
+        self.reason = reason
+        self.headers = headers
+        self.body = body
+
 
 if __name__ == '__main__':
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-    name = sys.argv[3]
+    my_host = sys.argv[1]
+    my_port = int(sys.argv[2])
+    my_name = sys.argv[3]
 
-    serv = MyHTTPServer(host, port, name)
+    serv = MyHTTPServer(my_host, my_port, my_name)
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
