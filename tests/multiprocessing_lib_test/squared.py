415484cc8675af3ba79e100df8d6e702d455e558
av.korovaev <av.korovaev@tensor.ru>
2020-02-20 15:13:37 +0300

    работа над многопоточностью фласка

--- tests/multiprocessing_lib_test/squared.py
+++ tests/multiprocessing_lib_test/squared.py
@@ -1,14 +1,16 @@
 import multiprocessing
 import time
 
+
 def multiprocessing_func(x):
     time.sleep(2)
     print(f'for {x} squared is {x*x}')
 
+
 if __name__ == '__main__':    
-    starttime = time.time()
+    start_time = time.time()
     processes = []
-    for i in range(0,10):
+    for i in range(0, 10):
         # multiprocessing_func(i)
         p = multiprocessing.Process(target=multiprocessing_func, args=(i,))
         processes.append(p)
@@ -17,4 +19,4 @@ if __name__ == '__main__':
     for process in processes:
         process.join()
 
-    print(f'time spended: {time.time() - starttime} second')
+    print(f'time spended: {time.time() - start_time} second')
