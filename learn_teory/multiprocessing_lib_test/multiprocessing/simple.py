1cb60bd5a0c106be6c75180025190e23601710bb
av.korovaev <av.korovaev@tensor.ru>
2020-03-02 18:05:40 +0300

    начал изучение многопроцессорности

---
+++ learn_teory/multiprocessing_lib_test/multiprocessing/simple.py
@@ -0,0 +1,31 @@
+import multiprocessing
+import time
+
+
+def square(val):
+    time.sleep(2)
+    print(f'process id is {multiprocessing.current_process().pid}')
+    print(f'process id is {multiprocessing.current_process().name}\n')
+    print(val*val)
+
+
+def cube(val):
+    time.sleep(2)
+    print(f'process id is {multiprocessing.current_process().pid}')
+    print(f'process id is {multiprocessing.current_process().name}\n')
+    print(val*val*val)
+
+
+if __name__ == '__main__':
+    p1 = multiprocessing.Process(target=square, args=(5,))
+    p2 = multiprocessing.Process(target=cube, args=(5,))
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
+
+    print('finish')
+
+
