86aadaf15e387e77d54423443a5fb56f1d5d3173
= <=>
2020-03-02 20:07:52 +0300

    работа над многопроцессорной версией

--- learn_teory/multiprocessing_lib_test/multiprocessing/simple.py
+++ learn_teory/multiprocessing_lib_test/multiprocessing/simple.py
@@ -5,18 +5,21 @@ import time
 def square(val):
     time.sleep(2)
     print(f'process id is {multiprocessing.current_process().pid}')
-    print(f'process id is {multiprocessing.current_process().name}\n')
+    print(f'process name is {multiprocessing.current_process().name}\n')
     print(val*val)
 
 
 def cube(val):
     time.sleep(2)
     print(f'process id is {multiprocessing.current_process().pid}')
-    print(f'process id is {multiprocessing.current_process().name}\n')
+    print(f'process name is {multiprocessing.current_process().name}\n')
     print(val*val*val)
 
 
 if __name__ == '__main__':
+    print(f'process id is {multiprocessing.current_process().pid}')
+    print(f'process name is {multiprocessing.current_process().name}\n')
+
     p1 = multiprocessing.Process(target=square, args=(5,))
     p2 = multiprocessing.Process(target=cube, args=(5,))
 
