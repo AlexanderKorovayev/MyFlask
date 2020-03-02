2c633df0c1d5b322ff14fa2ad9ff6a0a10b63834
av.korovaev <av.korovaev@tensor.ru>
2020-03-02 11:18:04 +0300

    отладка многопоточной версии

--- learn_teory/socket_lib_test/threading_server.py
+++ learn_teory/socket_lib_test/threading_server.py
@@ -16,27 +16,31 @@ sockobj.listen(5)
 def now():
     return datetime.now().time()
 
-def handle_client(connection):
+
+def handle_client(connection, lock):
     print(f'in handle, thread {threading.current_thread().name} start at {now()}\n')
-    time.sleep(10)
-    print(f'in handle, thread {threading.current_thread().name} at {now()}\n')
-    while True:
-        data = connection.recv(1024)
-        if not data:
-            break
-        reply = 'Echo=>{} at {}'.format(data, now())
-        connection.send(reply.encode())
-    connection.close()
+    print(f'in handle, thread {threading.current_thread().name} about lock at {now()}\n')
+    with lock:
+        print(f'in handle, thread {threading.current_thread().name} has lock at {now()}\n')
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            reply = 'Echo=>{} at {}'.format(data, now())
+            connection.send(reply.encode())
+        time.sleep(3)
+        connection.close()
+        print(f'in handle, thread {threading.current_thread().name} has release at {now()}\n')
     print(f'in handle, thread {threading.current_thread().name} finish at {now()}\n')
 
 
 def dispatcher():
+    lock = threading.Lock()
     while True:
         print(f'in main, thread name is {threading.current_thread().name} at {now()}\n')
         connection, address = sockobj.accept()
-        print('Server connected by', address, end=' ')
-        print('at', now())
-        threading.Thread(target=handle_client, args=(connection,)).start()
+        print(f'Server connected by {address} at {now()}')
+        threading.Thread(target=handle_client, args=(connection, lock)).start()
 
 
 dispatcher()
