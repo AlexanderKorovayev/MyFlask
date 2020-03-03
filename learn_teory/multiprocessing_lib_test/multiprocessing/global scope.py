ee4848c8d16c473edd2996efca398e56737d3611
av.korovaev <av.korovaev@tensor.ru>
2020-03-03 13:32:37 +0300

    изучение многопроцессорности

--- learn_teory/multiprocessing_lib_test/multiprocessing/global scope.py
+++ learn_teory/multiprocessing_lib_test/multiprocessing/global scope.py
@@ -1,29 +1,33 @@
 import multiprocessing 
-  
+import threading
+
 # empty list with global scope 
 result = [] 
-  
-def square_list(mylist): 
+
+
+def square_list(mylist):
     """ 
     function to square a given list 
     """
-    global result 
+
     # append squares of mylist to global list result 
     for num in mylist: 
         result.append(num * num) 
-    # print global list result 
-    print(f"Result(in process p1): {result}") 
+    # print global list result
+    print(f"Result in {multiprocessing.current_process().name}: {result}")
   
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     # input list 
-    mylist = [1,2,3,4] 
+    mylist = [1, 2, 3, 4]
   
     # creating new process 
-    p1 = multiprocessing.Process(target=square_list, args=(mylist,)) 
+    p1 = multiprocessing.Process(target=square_list, args=(mylist,))
+    # p1 = threading.Thread(target=square_list, args=(mylist,))
     # starting process 
     p1.start() 
     # wait until process is finished 
     p1.join() 
   
     # print global result list 
-    print("Result(in main program): {}".format(result)) 
+    print(f"Result in {multiprocessing.current_process().name}: {result}")
