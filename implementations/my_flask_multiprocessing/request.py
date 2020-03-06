74a77460dfae6a76b12a0df9ce5e50c758434e68
av.korovaev <av.korovaev@tensor.ru>
2020-03-06 16:50:32 +0300

    начал работу над многопроцессорной версией фласка

---
+++ implementations/my_flask_multiprocessing/request.py
@@ -0,0 +1,24 @@
+from interfaces.threaded.i_request_thread import IRequest
+from urllib.parse import parse_qs, urlparse
+
+
+class Request(IRequest):
+    """
+    класс объект запроса
+    """
+
+    def body(self):
+        size = self.headers.get('Content-Length')
+        if not size:
+            return None
+        return self._rfile.read(size)
+
+    def url(self):
+        return urlparse(self._target)
+
+    def path(self):
+        return self.url().path
+
+    def query(self):
+        return parse_qs(self.url().query)
+
---
+++ implementations/my_flask_multiprocessing/response.py
@@ -0,0 +1,10 @@
+from interfaces.threaded.i_response_thread import IResponse
+
+
+class Response(IResponse):
+    """
+    класс объект ответа
+    """
+
+    pass
+
