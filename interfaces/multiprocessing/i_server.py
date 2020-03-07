304b1e6e4ddf61637802d0229f771fd18bb3e64b
av.korovaev <av.korovaev@tensor.ru>
2020-03-07 14:42:23 +0300

    добавил работу с задачами

---
+++ interfaces/multiprocessing/i_server.py
@@ -0,0 +1,92 @@
+"""
+Модуль описывает базовую логику http сервера
+"""
+
+import socket
+from interfaces.threaded.i_request_thread import IRequest
+from interfaces.threaded.i_response_thread import IResponse
+import utils
+import threading
+from datetime import datetime
+
+
+class IServer:
+    """
+    класс, определяющий базовый функционал для сервера
+    """
+    def __init__(self, host_name, port_id, server_name, request, response):
+        self.port = port_id
+        self.server_name = server_name
+        self._host = host_name
+        if not (utils.check_type(request, IRequest)):
+            raise Exception('объект реквеста не соответствует заданным стандартам IRequest')
+        if not (utils.check_type(response, IResponse)):
+            raise Exception('объект респонса не соответствует заданным стандартам IResponse')
+        self._request = request
+        self._response = response
+
+    def serve_forever(self):
+        """
+        главная функция по обслуживанию клиента
+        """
+        serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            serv_sock.bind((self._host, self.port))
+            serv_sock.listen()
+
+            while True:
+                conn, _ = serv_sock.accept()
+                threading.Thread(target=self._serve_client, args=(conn,)).start()
+                
+        finally:
+            serv_sock.close()
+
+    def _serve_client(self, conn):
+        """
+        обслуживание запроса(обработка запроса, выполнение запроса, ответ клиенту)
+        :param conn: соединение с клиентом
+        """
+        print(f'start at {datetime.now().time()}')
+        try:
+            request = self._parse_request(conn)
+            response = self._handle_request(request)
+            self._send_response(conn, response)
+        except ConnectionResetError:
+            conn = None
+        except Exception as e:
+            self._send_error(conn, e)
+
+        if conn:
+            conn.close()
+        print(f'finish at {datetime.now().time()}')
+
+    def _parse_request(self, conn):
+        """
+        разбор запроса от клиента
+        :param conn:
+        :return: объект запроса
+        """
+        raise NotImplementedError
+
+    def _handle_request(self, request):
+        """
+        обработка запроса от клиента
+        :return: данные для клиента
+        """
+        raise NotImplementedError
+
+    def _send_response(self, conn, response):
+        """
+        Отправка ответа клиенту
+        :param conn: сокет
+        """
+        raise NotImplementedError
+
+    def _send_error(self, conn, err):
+        """
+        конструирование объекта ошибки и его отправка
+        :param conn: сокет
+        :param err: ошибка
+        """
+        raise NotImplementedError
--- utils.py
+++ utils.py
@@ -2,6 +2,10 @@
 модуль для вспомогательных функций
 """
 
+import multiprocessing
+from queue import Queue
+import time
+
 
 def check_type(obj, base_type):
     """
@@ -29,3 +33,49 @@ def check_type(obj, base_type):
                 return rez
     return rez
 
+
+def task(sec):
+    print('start task')
+    time.sleep(sec)
+    print('finish task')
+
+
+def task_listener(queue: multiprocessing.Queue):
+    while True:
+        task = queue.get()
+        task()
+
+
+def create_process():
+    """
+    функция проверяет возможнеость создания процесса и создаёт его если это возможно либо помещает в очередь
+    :return:
+    """
+
+    # создаю общую для потоков очередь, каждый поток создаётся и начинает работать бесконечно пока к нему не придёт
+    # новая задача
+    # задачи будут находиться в очереди из которой будут доставаться задачи
+
+    # создадим очередь из максимум 100 задач
+    queue = multiprocessing.Queue(100)
+    processes = []
+    for i in range(multiprocessing.cpu_count()):
+        # передаём сюда прогу которая будет бесконечно ждать задачу,
+        # эту задачу процесс будет забирать из очереди.
+        p = multiprocessing.Process(target=task_listener, args=(queue,))
+        p.start()
+        p.join()
+        processes.append(p)
+
+    # главный поток получает задачу и помещает её в очередь
+    # заполним сами очередь из заданий
+    for i in range(8):
+        queue.put(task(i))
+
+    # в очереди будет содержаться таск и пид процесса который должен его обработать, все процессы вытаскивают пид и сверяют со своим
+
+    # очередь потоко безопасна пока один процесс считывает из неё, остальные не могут обратиться к очереди(ПРОВЕРИТЬ!)
+    # процесс взял из очереди задачу и начал выполнять её , соответственно он не возьмёти ещё задачу пока не выполнит взятую
+
+
+create_process()
