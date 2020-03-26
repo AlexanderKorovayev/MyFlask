5dad876c2eafd0e4fcaa9c88839225de8f74e019
av.korovaev <av.korovaev@tensor.ru>
2020-03-26 11:24:08 +0300

    закончил изучение асинхронности

--- learn_teory/asyncio/book/tcp_echo_server.py
+++ learn_teory/asyncio/book/tcp_echo_server.py
@@ -19,11 +19,9 @@ async def echo(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         while True: 
             data = await reader.read(100)
             if data:
-                print(type(data))
-                print(f'data is {data}')
                 # проверим, что такой сервер способен принимать запросы пока загружен
-                await asyncio.sleep(1)
-                test()
+                await asyncio.sleep(2)
+                # test()
                 writer.write(data.upper())
                 # функция которая не позволяет писать в переполненный буфер
                 # и ждёт момента когда снова можно будет писать если буфер переполнился
@@ -31,9 +29,11 @@ async def echo(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
             else:
                 break
         print('leaving connection')
+    # если есть подключение, то сервер создаёт таск, который закрывается если закрыть и сам сервер
     except asyncio.CancelledError:
         msg = 'connection dropped'
         print(msg)
+        # но на этом этапе создаётся ещё один таск, который уже не попадёт в автоматический механизм закрытия
         asyncio.create_task(send_event(msg))
 
 
@@ -42,9 +42,7 @@ async def main(host='127.0.0.1', port=8888):
     server = await asyncio.start_server(echo, host, port)
 
     async with server:
-        print('in')
         await server.serve_forever()
-        print('in1')
 
 if __name__ == '__main__':
     try:
