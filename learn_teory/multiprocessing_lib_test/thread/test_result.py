63cfad95c76110f8c0bf09b9c6346153260a832a
= <=>
2020-04-18 17:30:47 +0300

    изучаю способы получения результата созданного потока

--- learn_teory/multiprocessing_lib_test/thread/test_result.py
+++ learn_teory/multiprocessing_lib_test/thread/test_result.py
@@ -1,11 +1,33 @@
 import threading
+from concurrent.futures import ThreadPoolExecutor
+from queue import Queue
 
 
 def test():
     return 5
 
+def test_with_queue(queue: Queue):
+    queue.put(5)
+
 
 if __name__ == '__main__':
+    # так результат не получить
+    '''
     t = threading.Thread(target=test)
     test = t.start()
     print(test)
+    '''
+    # а так получить
+    '''
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        submit_object = executor.submit(test)
+    res = submit_object.result()
+    print(res)
+    '''
+    # и так тоже
+    '''
+    queue = Queue()
+    p = threading.Thread(target=test_with_queue, args=(queue,))
+    p.start()
+    print(queue.get())
+    '''
--- learn_teory/multiprocessing_lib_test/thread/thread_pool_executor.py
+++ learn_teory/multiprocessing_lib_test/thread/thread_pool_executor.py
@@ -16,3 +16,4 @@ if __name__ == "__main__":
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
         executor.map(thread_function, range(3))
+    
